bb08f41dee3a89b549c24f735c0e15215a8c4494
William <coby.kelly.ck@gmail.com>
2019-07-18 21:18:06 -0400

    hey

---
+++ Practice Exercises C#/TheOrderOfThings.sln/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TheOrderOfThings.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            String opinion, size, age, shape, color, origin, material, purpose;
+            String noun;
+
+            number = 5 ;
+            opinion = " Awesome ";
+            size = "Teensy-Weensy ";
+            age = "new ";
+            shape = "oblong ";
+            color = "\tbrightorange ";
+            origin = "\tAlphaCentaurian ";
+            material = "platinum ";
+            purpose = "good ";
+
+            noun = "dragons ";
+
+            Console.WriteLine(number + opinion + size + age + shape + color + origin + material + purpose + noun);
+        }
+    }
+}
