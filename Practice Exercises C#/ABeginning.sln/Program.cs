eab60c3890a86cab9bab2ab425c00b443fb95254
spookur <spookur>
2019-07-22 13:07:52 -0400

    something

---
+++ Practice Exercises C#/ABeginning.sln/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABeginning.sln
+{
+    class ABeginning
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the guild");
+            Console.WriteLine("Typing code is easier than I thought...");
+            Console.WriteLine("Typity Typity Tyle!");
+            Console.WriteLine("After is finish typing,");
+            Console.WriteLine("I'll build the solution using the build Menu,");
+            Console.WriteLine("and I'll run it using the Debug Menu");
+            Console.WriteLine("so that my brilliant words will appear in the console!");
+            Console.WriteLine("And it all starts with \'Hello, World!\'");
+
+
+        }
+    }
+}
---
+++ Practice Exercises C#/PracticeMath.sln/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PracticeMath.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 5;
+            int y = 10;
+            int z = 20;
+
+            Console.WriteLine(Add(Add(, y), Add(x, Add(y, z)));
+        }
+    }
+}
