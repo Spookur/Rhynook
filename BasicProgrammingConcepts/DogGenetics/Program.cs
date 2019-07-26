c5e92fc340abefd2d375a02263efb8abce0a6333
unknown <coby.kelly.ck@gmail.com>
2019-07-25 22:04:23 -0400

    conceptsFromLaptop

---
+++ BasicProgrammingConcepts/DogGenetics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogGenetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName;
+
+            Console.WriteLine("What is the name of your dog?");
+            dogName = Console.ReadLine();
+
+            Console.WriteLine("Well then, I have this very reliable report on " + dogName + "!");
+            Console.ReadLine();
+
+            Console.WriteLine(dogName + " is:");
+            Console.WriteLine("43% Bichon Frise");
+            Console.WriteLine("7% Chihuahua");
+            Console.WriteLine("25% Shih Tzu");
+            Console.WriteLine("9% Yorkie");
+            Console.WriteLine("6% Chinese Ankle Biter");
+            Console.ReadLine();
+        }
+    }
+}
