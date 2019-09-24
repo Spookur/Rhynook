ebd63aca529812e29f1a1ab654c4e1fac4784979
William <coby.kelly.ck@gmail.com>
2019-09-24 17:34:25 -0400

    hiSaunders

--- MILESTONE 2/VulpixManager/VulpixManager.Models/Pokemon.cs
+++ MILESTONE 2/VulpixManager/VulpixManager.Models/Pokemon.cs
@@ -11,37 +11,9 @@ namespace PokemonManager.Models
     
         
         public int Id { get; set; }
-
-        private string _name;
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                 
-            }
-        }
-
-        private string _pokeType;
-        public string PokeType
-        {
-            get { return _pokeType; }
-            set
-            {
-
-            }
-        }
-
-        private string _description;
-        public string Description
-        {
-            get { return _description; }
-            set
-            {
-
-            }
-        }
-
+        public string Name { get; set; }
+        public string PokeType { get; set; }
+        public string Description { get; set; }
         
         public Pokemon()
         {
